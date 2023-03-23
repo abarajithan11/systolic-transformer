4b67eb3b8b1547fe7826c59c6d31919fd352a14c
Aba <abarajithan07@gmail.com>
2023-03-22 23:17:35 -0700

    Update norm & pins

--- dual_core_opt/pnr/pinPlacement.tcl
+++ dual_core_opt/pnr/pinPlacement.tcl
@@ -1,13 +1,13 @@
 getPinAssignMode -pinEditInBatch -quiet
 setPinAssignMode -pinEditInBatch true
 
-editPin -pinWidth 0.2 -pinDepth 0.6 -fixOverlap 1 -unit MICRON -spreadDirection clockwise -side Left -layer 3 -spreadType center -spacing 0.8 -pin {clk* rst* inst_core* mem_in_*}
+editPin -pinWidth 0.2 -pinDepth 0.6 -fixOverlap 1 -unit MICRON -spreadDirection clockwise -side Left -layer 3 -spreadType center -spacing 0.8 -pin {clk* core_gate* rst* inst_core* mem_in_*}
 
 setPinAssignMode -pinEditInBatch false
 getPinAssignMode -pinEditInBatch -quiet
 setPinAssignMode -pinEditInBatch true
 
-editPin -pinWidth 0.2 -pinDepth 0.6 -fixOverlap 1 -unit MICRON -spreadDirection counterclockwise -side Bottom -layer 3 -spreadType center -spacing 0.8 -pin {norm_gate s_valid* norm_valid psum_norm* out_core* }
+editPin -pinWidth 0.2 -pinDepth 0.6 -fixOverlap 1 -unit MICRON -spreadDirection counterclockwise -side Bottom -layer 3 -spreadType center -spacing 0.8 -pin {s_valid* norm_valid psum_norm* out_core* }
 
 setPinAssignMode -pinEditInBatch false
 
